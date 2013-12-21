02f07d01151c488c2ef1784226849ca1d53fb647
LLLLKKKK <kan.liu.229@gmail.com>
2013-12-21 23:11:38 +0800

    image..

--- cv_site/static/js/main.js
+++ cv_site/static/js/main.js
@@ -12,3 +12,27 @@ search_btn.click(function(e) {
 	e.preventDefault();
 	image_input.click();
     });
+
+
+$("#image-container").autobrowse({
+	url: function (offset)
+	    {
+		return "http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=?";
+	    },
+	    template: function (response)
+	    {
+		var markup='';
+		for (var i=0; i<response.items.length; i++)
+		    {
+			markup+='<a href="'+response.items[i].link+'"><img class="result-image" src="'+response.items[i].media.m+'" /></a>'
+			    };
+		return markup;
+	    },
+	    loader: '<div class="loader"></div>',
+	    itemsReturned: function (response) { return response.items.length; },
+	    offset: 0,
+	    max: 100,
+	    useCache: false,
+	    expiration: 1
+	    }
+    );
