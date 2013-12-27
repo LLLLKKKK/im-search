594b9fe72229cbcceceb909cea221b8217a0a284
LLLLKKKK <kan.liu.229@gmail.com>
2013-12-27 19:37:39 +0800

    fix ...

--- cv_site/static/js/main.js
+++ cv_site/static/js/main.js
@@ -11,29 +11,29 @@ image_input.change(function(e) {
 	reader = new FileReader();
 	file = image_input.get(0).files[0];
 	reader.readAsDataURL(file);
-	formdata.append("image", file);	
+	formdata.append("image", file);
 	formdata.append("csrfmiddlewaretoken", csrf_input.attr('value'));
 	$.ajax({
-                type: "POST",
-		    url: "search",
-		    processData: false,
-		    contentType: false,
-		    data: formdata
-		    }).done(function(msg) {
-			    msg = JSON.parse(msg);
-			    console.log(msg);
-			    if (msg['error']) {
-				error_label.text(msg['error']);
-				error_label.parent().addClass('has-error');
-				return ;
-			    }
-			    error_label.parent().removeClass('has-error');
-			    error_label.text('');
-			    if (msg['result']) {
-				console.log(msg['result']);
-			    }
-			}); 
-    });
+		type: "POST",
+		url: "search",
+		processData: false,
+		contentType: false,
+		data: formdata
+	}).done(function(msg) {
+		msg = JSON.parse(msg);
+		console.log(msg);
+		if (msg['error']) {
+			error_label.text(msg['error']);
+			error_label.parent().addClass('has-error');
+		return ;
+		}
+			error_label.parent().removeClass('has-error');
+			error_label.text('');
+		if (msg['result']) {
+			console.log(msg['result']);
+		}
+	});
+});
 
 search_btn.click(function(e) {
 	e.preventDefault();
@@ -42,24 +42,20 @@ search_btn.click(function(e) {
 
 
 $("#image-container").autobrowse({
-	url: function (offset)
-	    {
+	url: function (offset){
 		return "http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=?";
-	    },
-	    template: function (response)
-	    {
+	},
+	template: function (response) {
 		var markup='';
-		for (var i=0; i<response.items.length; i++)
-		    {
-			markup+='<a href="'+response.items[i].link+'"><img class="result-image" src="'+response.items[i].media.m+'" /></a>'
-			    };
+		for (var i=0; i<response.items.length; i++){
+			markup+='<a href="'+response.items[i].link+'"><img class="result-image" src="'+response.items[i].media.m+'" /></a>';
+		}
 		return markup;
-	    },
-	    loader: '<div class="loader"></div>',
-	    itemsReturned: function (response) { return response.items.length; },
-	    offset: 0,
-	    max: 100,
-	    useCache: false,
-	    expiration: 1
-	    }
-    );
+	},
+	loader: '<div class="loader"></div>',
+	itemsReturned: function (response) { return response.items.length; },
+	offset: 0,
+	max: 100,
+	useCache: false,
+	expiration: 1
+});
